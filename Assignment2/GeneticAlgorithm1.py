ae2918b45ad8abe7bcd814b5e3f05e2bfcde8c85
Ray Wang <rwang3@wpi.edu>
2015-09-19 15:36:16 -0400

    I can't spell things
    This should probably work

--- Assignment2/GeneticAlgorithm1.py
+++ Assignment2/GeneticAlgorithm1.py
@@ -1,9 +1,15 @@
+import csv
+import math
+
 class GeneticAlgorithm1
     #Give this function a filename and delimiter
+    target = 0
+    population = []
+    
     def readcsv(self, name):
         temp_input = []
             with open( name, 'rb') as csvfile:
-                map_reader = csv.reader(csvfile, delimiter='\n', quotechar='|')
+                input_reader = csv.reader(csvfile, delimiter='\n', quotechar='|')
                 for row in map_reader:
-                    temp_map.append(row)
-            self._raw_map = temp_map
+                    temp_input.append(row)
+            self._raw_map = temp_input
